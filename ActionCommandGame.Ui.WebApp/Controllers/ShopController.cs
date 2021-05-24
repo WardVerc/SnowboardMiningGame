da9ef23eda99103de267472c63639c3f061934ae
Ward Vercruyssen <r0435477@student.vives.be>
2021-05-24 16:55:28 +0200

    Added shopitems in the Shop, showing current balance. Also showing current level of the player in home screen.

--- ActionCommandGame.Ui.WebApp/Controllers/ShopController.cs
+++ ActionCommandGame.Ui.WebApp/Controllers/ShopController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using ActionCommandGame.Services.Abstractions;
+using ActionCommandGame.Ui.WebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ActionCommandGame.Ui.WebApp.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly IItemService _itemService;
+        private readonly IPlayerService _playerService;
+
+        public ShopController(IItemService itemService, IPlayerService playerService)
+        {
+            _itemService = itemService;
+            _playerService = playerService;
+        }
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            //Get all items, sorted by Price
+            var allItems = _itemService.Find().OrderBy(i=>i.Price).ToList();
+            var currentPlayer = _playerService.Find().SingleOrDefault(p => User.Identity != null && p.Name == User.Identity.Name);
+            
+            var model = new ItemsShopViewModel()
+            {
+                AllItems = allItems,
+                CurrentPlayer = currentPlayer
+            };
+            return View(model);
         }
     }
 }
---
+++ ActionCommandGame.Ui.WebApp/ViewModels/ItemsShopViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ActionCommandGame.Model;
+
+namespace ActionCommandGame.Ui.WebApp.ViewModels
+{
+    public class ItemsShopViewModel
+    {
+        public IList<Item> AllItems { get; set; }
+        public Player CurrentPlayer { get; set; }
+    }
+}
